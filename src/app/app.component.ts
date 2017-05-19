9b4e4fe980031a8fbe1bc55ca534bdd78dc88471
Andre Passos <andre-ap2@hotmail.com>
2017-05-18 21:45:42 -0300

    dc.js example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+
+import * as d3 from 'd3';
+import * as dc from 'dc';
+import * as crossfilter from 'crossfilter2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'app works!';
+
+  @ViewChild('test') test: ElementRef;
+  chart: dc.BarChart;
+
+  /**
+   *
+   */
+  constructor() {
+  }
+
+  ngAfterViewInit() {
+    this.chart = dc.barChart(this.test.nativeElement);
+
+    let experiments = [];
+    d3.json('assets/data.json').get((err: any, data: any) => {
+      experiments = data;
+      let ndx = crossfilter(experiments);
+      let runDimension = ndx.dimension(d => +d.run);
+      let speedSumGroup = runDimension.group().reduceSum(d => d.speed * d.run / 1000);
+      this.chart.width(768)
+        .height(480)
+        .x(d3.scale.linear().domain([6, 20]))
+        .brushOn(false)
+        .yAxisLabel("This is the Y Axis!")
+        .dimension(runDimension)
+        .group(speedSumGroup)
+        .on('renderlet', chart => {
+          chart.selectAll('rect').on("click", d => {
+            console.log("click!", d);
+          });
+        });
+
+        this.chart.render();
+    });
+  }
+
+  ngOnInit(): void {    
+  }
 }
